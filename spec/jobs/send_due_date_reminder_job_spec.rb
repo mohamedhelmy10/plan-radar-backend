4a14a44a453f9f12b016773ba8534e6d57216b13
Mohamed Elsayed <mohamedhelmy51288@gmail.com>
2024-12-15 23:29:33 +0200

    update perform_at call

--- spec/jobs/send_due_date_reminder_job_spec.rb
+++ spec/jobs/send_due_date_reminder_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SendDueDateReminderJob, type: :job do
   let(:ticket) { create(:ticket) }
 
   it 'sends an email reminder when the job is performed' do
-    expect { SendDueDateReminderJob.perform_at(ticket.id, Time.now) }
+    expect { SendDueDateReminderJob.perform_at(Time.now, ticket.id) }
       .to change { ActionMailer::Base.deliveries.count }.by(1)
   end
 end
